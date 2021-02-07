c3e27e889fca8efa5aede3b38f994a789f598a1c
Cyprien <cyprienthao@gmail.com>
2021-02-07 21:27:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require("dotenv").config();
 const express = require("express")
 const bodyParser = require("body-parser")
 const db = require("./app/models")
+const cors = require("cors")
+
 
 app = express();
+
+
 db.sequelize.sync({ force: false });
 
+var corsOptions = {
+    origin: "*",
+};
+app.use(cors(corsOptions));
 // pars
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 // parse requests of content-type - application/x-www-form-urlencoded (Not necessary here I think)
