90d27f315e9981b2673bd63e9217490a45ec0ccf
Cyprien <cyprienthao@gmail.com>
2021-02-08 23:52:52 +0100

    Update quizz.routes.js

--- app/routes/quizz.routes.js
+++ app/routes/quizz.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
     const Quizz = require('../controllers/quizz.controller.js')
     const authJwt = require("../middlewares/authJwt.middleware")
 
-    app.post('/quizz/create', authJwt.verifyToken, Quizz.createQuizz)
+    app.post('/quizz/create', Quizz.createQuizz)
     app.put('/quizz/edit/:quizzId', Quizz.editQuizz)
     app.get('/quizz', Quizz.getAllQuizz)
     app.get('/quizz/:quizzId', Quizz.getQuizzById)
