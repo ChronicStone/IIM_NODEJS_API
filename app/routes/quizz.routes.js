312454ee7366d269c04031bc12a5ead6e40c6dfd
Cyprien <cyprienthao@gmail.com>
2021-02-09 10:13:19 +0100

    Update quizz.routes.js

--- app/routes/quizz.routes.js
+++ app/routes/quizz.routes.js
@@ -5,6 +5,7 @@ module.exports = (app) => {
     app.post('/quizz/create', authJwt.verifyToken, Quizz.createQuizz)
     app.put('/quizz/edit/:quizzId', Quizz.editQuizz)
     app.get('/quizz', Quizz.getAllQuizz)
+    app.get('/player-quizz/:playerId', Quizz.getQuizzByCreatorId)
     app.get('/quizz/:quizzId', Quizz.getQuizzById)
     app.delete('/quizz/disable/:quizzId', Quizz.disableQuizz)
     app.delete('/quizz/delete/:quizzId', Quizz.deleteQuizz)
